d0e6c4ce468e7ba254de140698f4cdc353fb14c7
dewecorp <ibnuhasan3@gmail.com>
2026-06-29 11:01:23 +0700

    update baru

--- config/app_commit.php
+++ config/app_commit.php
@@ -1,9 +1,5 @@
 <?php
-/**
- * Commit SHA versi kode yang terpasang di server ini.
- * Diperbarui otomatis setelah Update Sistem dari GitHub.
- */
 return [
-    'sha' => '6e372607e594188f3635a33f20b67ac9edfa3153',
-    'date' => '2026-06-29T01:41:22Z',
+    'sha' => '30e1612d8e0f89cc70f4ae5ac7ad3bda15a49fdb',
+    'date' => '2026-06-29T03:43:21Z',
 ];
